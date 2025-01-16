fbf03b04547507e7f18b89336089529eba018d5a
Yogesh Pathe <98703537+yugyogeshpawar@users.noreply.github.com>
2025-01-16 20:20:26 +0530

    Create 1.basics.py

---
+++ numpy/1.basics.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array([1.2, 2.1, 3.5, 4.1])
+
+print(arr[0:3])
+
+
+# int       1, 2, 3, 4, 5, 6
+# char      a, b, c, d, e, f
+# float     1.5, 1.0, 2.5, 6.7
+# string    "India", "Prinyanshu", "Class", "Python"
+# bool      True, False
+
+
+
